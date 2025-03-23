ea67e2e6c18e8472aa229965b32f93930a4ebe4b
Ilsa <irehmat123@gmail.com>
2025-03-23 16:40:49 -0400

    did question 24 to 28

--- AutomatonController.java
+++ AutomatonController.java
@@ -4,6 +4,32 @@
  * @author David J. Barnes and Michael Kölling
  * @version  2016.02.29
  */
+/*
+ * Answers for Lab 7 part 3
+ * 
+ * Question 24
+ * Output of the initial state: A line with * (indicating the initial active cell).
+ * After calling step: The pattern evolves as the state transitions.
+ * After calling run: The automaton continuously runs and shows
+ * a dynamic pattern over time.
+ * 
+ * Question 25
+ * The step method pattern is the same but the run method pattern changed.
+ * 
+ * Question 26
+ * There are two versions of the fill method in the java.util.Arrays class
+ * that take an int[] parameter. One fills the entire array with a 
+ * specified value, and the other fills a specified range of the array.
+ * The reset method of Automaton likely uses Arrays.fill to reset the state
+ * array to a specific value, usually 0.
+ * 
+ * Question 27
+ * I did not understand.
+ * 
+ * Question 28
+ * 
+ * 
+ */
 public class AutomatonController
 {
     // The automaton.
